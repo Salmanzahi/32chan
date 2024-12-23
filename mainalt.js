51e8e8567fb33e6f99a358c938ace8d301ae3eba
SalmanZahi <87903013+Salmanzahi@users.noreply.github.com>
2024-12-23 22:02:17 +0700

    fix like reload

--- mainalt.js
+++ mainalt.js
@@ -341,6 +341,7 @@ function showMessages(sortOrder = 'desc') {
                 messagesList.appendChild(li);
 
                 loadReplies(message.id);
+                loadUserMessages(); // Load user messages after showing all messages
             });
         });
     } else {

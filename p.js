7d20f986728903c022436de1d47ef36d954d1a95
SalmanZahi <87903013+Salmanzahi@users.noreply.github.com>
2025-01-02 07:19:08 +0700

    ajfajdfppma

--- p.js
+++ p.js
@@ -1,13 +1,1 @@
-
-
-export const firebaseConfig = {
-    apiKey: "AIzaSyAYRcDk0UkOh9w7o1umQ_cDkPVxVOQojAw",
-    authDomain: "db-test-6a570.firebaseapp.com",
-    databaseURL: "https://db-test-6a570-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "db-test-6a570",
-    storageBucket: "db-test-6a570.appspot.com",
-    messagingSenderId: "1022540875615",
-    appId: "1:1022540875615:web:a76667f79e2d7cc21998ea",
-    measurementId: "G-80MH658TNG"
-};
-firebase.initializeApp(firebaseConfig);
+function _0x394d(_0x16af9a,_0x5e9993){const _0x349017=_0x3490();return _0x394d=function(_0x394d9d,_0x8523ea){_0x394d9d=_0x394d9d-0x1be;let _0x2b5413=_0x349017[_0x394d9d];return _0x2b5413;},_0x394d(_0x16af9a,_0x5e9993);}function _0x3490(){const _0x11deb3=['initializeApp','10FpqYZL','db-test-6a570.firebaseapp.com','1022540875615','4429053iHHZOs','6242628BDNhXo','3875145qRKFDZ','3947444SJUTqj','AIzaSyAYRcDk0UkOh9w7o1umQ_cDkPVxVOQojAw','16uTLydU','2747540DOtzTD','1422407wrpKYY','1:1022540875615:web:a76667f79e2d7cc21998ea','db-test-6a570','5ErtGYW','1057898pLnqql','db-test-6a570.appspot.com'];_0x3490=function(){return _0x11deb3;};return _0x3490();}const _0x2655dc=_0x394d;(function(_0x3acc0c,_0xb5e313){const _0x55003d=_0x394d,_0x58e8e2=_0x3acc0c();while(!![]){try{const _0x395941=-parseInt(_0x55003d(0x1ca))/0x1+-parseInt(_0x55003d(0x1c5))/0x2+parseInt(_0x55003d(0x1c1))/0x3+parseInt(_0x55003d(0x1c2))/0x4+-parseInt(_0x55003d(0x1c9))/0x5*(-parseInt(_0x55003d(0x1c0))/0x6)+-parseInt(_0x55003d(0x1c6))/0x7*(-parseInt(_0x55003d(0x1c4))/0x8)+parseInt(_0x55003d(0x1bf))/0x9*(-parseInt(_0x55003d(0x1cd))/0xa);if(_0x395941===_0xb5e313)break;else _0x58e8e2['push'](_0x58e8e2['shift']());}catch(_0x3b13eb){_0x58e8e2['push'](_0x58e8e2['shift']());}}}(_0x3490,0xc3b5f));export const firebaseConfig={'apiKey':_0x2655dc(0x1c3),'authDomain':_0x2655dc(0x1ce),'databaseURL':'https://db-test-6a570-default-rtdb.asia-southeast1.firebasedatabase.app','projectId':_0x2655dc(0x1c8),'storageBucket':_0x2655dc(0x1cb),'messagingSenderId':_0x2655dc(0x1be),'appId':_0x2655dc(0x1c7),'measurementId':'G-80MH658TNG'};firebase[_0x2655dc(0x1cc)](firebaseConfig);

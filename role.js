29ef4d104f956b9b1db5817f07c18d2ae89f1a20
SalmanZahi <87903013+Salmanzahi@users.noreply.github.com>
2025-02-10 21:20:01 +0700

    Update role.js

--- role.js
+++ role.js
@@ -2,6 +2,6 @@ export const adminRoles = {
     admins: [
         "y3FYZSmXAQV6mNdXcZ3yqXqXdC93", // Salman
         "vcfmzUKXBuQo3vQjA0n96Pqqm3D3", // Arya
-        "uid3"
+        "atek34BlFTcQgTq8sMHYhLBvQUB3" // Ratol
     ]
 };
